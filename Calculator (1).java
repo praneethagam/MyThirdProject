978f78cf6e5d4f085e48afc7d54d73d34afab43f
praneethagam <63359017+praneethagam@users.noreply.github.com>
2020-04-08 17:54:34 -0400

    made change
    hi

---
+++ Calculator (1).java
@@ -0,0 +1,41 @@
+package com;
+
+
+//Behavioral inheritance
+public abstract class Calculator implements CalculatorInterface {
+
+	
+	public int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1+num2;
+	}
+	public double add(int num1, double num2) {
+		// TODO Auto-generated method stub
+		return num1+num2;
+	}
+
+	
+	public int sub(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1-num2;
+	}
+
+	
+	public int mul(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1*num2;
+	}
+
+
+	@Override
+	public int div(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	
+	
+
+
+}
